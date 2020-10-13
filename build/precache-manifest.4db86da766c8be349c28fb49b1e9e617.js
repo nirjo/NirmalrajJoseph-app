07089fbaa9877948ae677584f108c4a6ad6a56bb
Nirmalraj Joseph <nirmalraj.joseph86@gmail.com>
2020-10-13 21:55:18 +0530

    add build

---
+++ build/precache-manifest.4db86da766c8be349c28fb49b1e9e617.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "27834142dea2694eadcef78f28f22177",
+    "url": "/index.html"
+  },
+  {
+    "revision": "de4a22b026283eaa2e85",
+    "url": "/static/css/main.1d52c094.chunk.css"
+  },
+  {
+    "revision": "1dc121569cd5e73c1ff3",
+    "url": "/static/js/2.6ed7c4dc.chunk.js"
+  },
+  {
+    "revision": "b688c54f9855505f47aa85b5f9c54246",
+    "url": "/static/js/2.6ed7c4dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de4a22b026283eaa2e85",
+    "url": "/static/js/main.1123163a.chunk.js"
+  },
+  {
+    "revision": "2903eb1f4c21d3b2e2e5",
+    "url": "/static/js/runtime-main.89ba0b7d.js"
+  },
+  {
+    "revision": "945bbe2329bd33ad9b86f193a17190f2",
+    "url": "/static/media/Maps-1.945bbe23.jpg"
+  },
+  {
+    "revision": "3b7a4417da9884d1c771fe19bef5eb8e",
+    "url": "/static/media/pr1.3b7a4417.png"
+  },
+  {
+    "revision": "eb040141abd40eada41e6cf334442708",
+    "url": "/static/media/pr2.eb040141.png"
+  },
+  {
+    "revision": "f71cd223911f9a0928d6cf9b6b7a782d",
+    "url": "/static/media/pr3.f71cd223.png"
+  }
+]);
